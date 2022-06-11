6029edec2585fb092dfc6bf9a8317deade67f3d2
Arseny <agent47daun@gmail.com>
2022-06-11 17:35:39 +0400

    Инициализация проекта + решение квадратных уравнений

---
+++ utils.py
@@ -0,0 +1,14 @@
+from math import sqrt
+from typing import Union
+
+
+def solve(a: float, b: float, c: float) -> Union[None, tuple, float]:
+    discriminant = pow(b, 2) - (4 * a * c)
+    if discriminant < 0:
+        return None
+    elif discriminant == 0:
+        return -(b / (2 * a))
+    else:
+        first_solution = ((-b) + sqrt(discriminant)) / 2
+        second_solition = ((-b) - sqrt(discriminant)) / 2
+        return (first_solution, second_solition)
